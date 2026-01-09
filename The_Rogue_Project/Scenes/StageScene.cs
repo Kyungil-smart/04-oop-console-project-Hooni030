ec22a53ffa45beee433deb615e83aa2be116e007
Hooni0_0 <startrack0515@gmail.com>
2026-01-09 12:46:15 +0900

    [Update] 스테이지 진입 시 로직 추가
    260109
    선택된 스테이지 난이도에 따라 초기 플레이어 스텟을 초기화 하는 매서드 추가

--- The_Rogue_Project/Scenes/StageScene.cs
+++ The_Rogue_Project/Scenes/StageScene.cs
@@ -1,21 +1,23 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Numerics;
+using System.Runtime.InteropServices;
 
 public class StageScene : Scene
 {
-    //public void SetSceneData(Tile[,], Vector playerStart, Monster[] monsters)
-    //{
+    private int _stageLevel;
 
-    //}
+    private Tile[,] _field = new Tile[10, 25];
 
-    private Tile[,] _field = new Tile[10, 20];
     private PlayerCharacter _player;
 
-    public StageScene(PlayerCharacter player) => Init(player);
+    public StageScene(PlayerCharacter player, int level)
+    {
+        _stageLevel = level;
+        Init(player);
+    }
 
     public void Init(PlayerCharacter player)
     {
         _player = player;
-
         for (int y = 0; y < _field.GetLength(0); y++)
         {
             for (int x = 0; x < _field.GetLength(1); x++)
@@ -26,16 +28,42 @@ public class StageScene : Scene
         }
     }
 
+    private void PlayerStatInit()
+    {
+        switch (_stageLevel)
+        {
+            case 0:
+                _player.MaxHp = 7;
+                _player.CurrentShield = 1;
+                _player.MaxShield = 3;
+                break;
+            case 1:
+                _player.MaxHp = 5;
+                _player.CurrentShield = 0;
+                _player.MaxShield = 3;
+                break;
+            case 2:
+                _player.MaxHp = 3;
+                _player.CurrentShield = 0;
+                _player.MaxShield = 2;
+                break;
+        }
+
+        _player.StatInit();
+        Debug.Log($"{_player.MaxHp}");
+    }
+
     public override void Enter()
     {
+        PlayerStatInit();
+
         _player.Field = _field;
-        _player.Position = new Vector(1, 1);
+        _player.Position = new Vector(5, 2);
         _field[_player.Position.Y, _player.Position.X].OnTileObject = _player;
     }
     public override void Update()
     {
         _player.Update();
-
     }
     public override void Render()
     {
@@ -53,7 +81,12 @@ public class StageScene : Scene
         {
             for (int x = 0; x < _field.GetLength(1); x++)
             {
-                _field[y, x].Print();
+                if (_field[y, x].OnTileObject == _player)
+                {
+                    _player.Symbol.Print(ConsoleColor.Black, ConsoleColor.Gray);
+                }
+                else
+                    _field[y, x].Print();
             }
             Console.WriteLine();
         }
