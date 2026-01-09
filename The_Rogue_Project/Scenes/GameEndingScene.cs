1e00ad79b89e29571af12db78fef3f04ca2e4bb8
Hooni0_0 <startrack0515@gmail.com>
2026-01-09 12:45:03 +0900

    [Create] 게임 종료 씬 생성
    260109
    플레이어 사망시 전환되는 게임 종료 씬 생성

---
+++ The_Rogue_Project/Scenes/GameEndingScene.cs
@@ -0,0 +1,18 @@
+﻿public class GameEndingScene : Scene
+{
+    public override void Enter()
+    {
+    }
+
+    public override void Exit()
+    {
+    }
+
+    public override void Render()
+    {
+    }
+
+    public override void Update()
+    {
+    }
+}
