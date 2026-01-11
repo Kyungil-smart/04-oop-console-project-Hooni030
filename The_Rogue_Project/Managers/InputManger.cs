974b734a5cd9999fb580aa9b2f8a230e95bbc748
Hooni0_0 <startrack0515@gmail.com>
2026-01-12 01:17:23 +0900

    [Update] 입력 및 화면  전환 변경
    260111
    프레임 마다 화면 출력으로 변경
    기존) 입력이 있어야 화면 전환
    신규) 입력이 없어도 일정 시간마다 화면 전환

---
+++ The_Rogue_Project/Managers/InputManger.cs
@@ -0,0 +1,51 @@
+﻿public static class InputManager
+{
+    private static ConsoleKey _current;
+
+    private static readonly ConsoleKey[] _Keys =
+    {
+        // 이동 : 상하좌우
+        ConsoleKey.UpArrow,
+        ConsoleKey.DownArrow,
+        ConsoleKey.LeftArrow,
+        ConsoleKey.RightArrow,
+
+        // 공격
+        ConsoleKey.Spacebar,
+
+        ConsoleKey.I, // 인벤토리
+
+        ConsoleKey.Enter, // 선택
+
+        ConsoleKey.L // 로그키
+    };
+
+    public static bool IsCorrectkey(ConsoleKey input)
+        => _current == input;
+
+    public static void GetUserInput()
+    {
+        while (Console.KeyAvailable)
+        {
+            ConsoleKey input = Console.ReadKey(true).Key;
+
+            foreach (ConsoleKey key in _Keys)
+            {
+                if (key == input)
+                {
+                    _current = key;
+                    break;
+                }
+            }
+        }
+    }
+    public static void ResetKey()
+        => _current = ConsoleKey.None;
+
+    public static ConsoleKey UsedKey()
+    {
+        ConsoleKey key = _current;
+        _current = ConsoleKey.None;
+        return key;
+    }
+}
